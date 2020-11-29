cdea35b3fcafab326a528133588c80a1e88bd860
liaozhongxun <869664233@qq.com>
2020-11-29 16:52:14 +0800

    first

---
+++ sidebars.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // someSidebar: {
+  //   Docusaurus: ['doc1', 'doc2', 'doc3'],
+  //   Features: ['mdx'],
+  // },
+
+    // docs: [
+    //   {
+    //     type: 'category',
+    //     label: 'Getting Started',
+    //     items: ['doc2','doc3'],
+    //   },
+    //   {
+    //     type: 'category',
+    //     label: 'Docusaurus',
+    //     items: ['doc1'],
+    //   },
+    // ],
+    
+    docs: {
+      Guides: [
+        // 'doc3',
+        {
+          type: 'category',
+          label: 'Docs',
+          items: ['doc1', 'doc2'],
+        },
+        {
+          type: 'category',
+          label: 'Docs2',
+          items: ['doc3'],
+        },
+      ],
+    },
+  
+};
